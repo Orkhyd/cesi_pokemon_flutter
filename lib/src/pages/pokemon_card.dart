3d14080eaa7da4aea04f2df68e680e633f02a264
Marius Roelly <m.roelly@opticod.fr>
2025-02-12 10:08:06 +0100

    Add add button to pokemon details

--- lib/src/pages/pokemon_card.dart
+++ lib/src/pages/pokemon_card.dart
@@ -9,58 +9,65 @@ class PokemonCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title:
-              Text(pokemon.name[0].toUpperCase() + pokemon.name.substring(1)),
-        ),
-        body: Padding(
+      appBar: AppBar(
+        title: Text(pokemon.name[0].toUpperCase() + pokemon.name.substring(1)),
+      ),
+      body: Padding(
           padding: const EdgeInsets.all(6),
           child: Card.outlined(
-              color: Colors.white,
-              child: Padding(
+            color: Colors.white,
+            child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      pokemon.name[0].toUpperCase() + pokemon.name.substring(1),
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    ),
-                    Divider(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.network(
-                          pokemon.frontImage,
-                          width: 400,
-                          height: 400,
-                        ),
-                      ],
-                    ),
-                    Text('HP : ${pokemon.hp}'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Atk : ${pokemon.atk}'),
-                        Text('Def : ${pokemon.def}'),
-                      ],
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        pokemon.name[0].toUpperCase() +
+                            pokemon.name.substring(1),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text('Sp. Atk : ${pokemon.speAtk}'),
-                          Text('Sp. Def : ${pokemon.speDef}'),
-                        ]),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          Image.network(
+                            pokemon.frontImage,
+                            width: 400,
+                            height: 400,
+                          ),
+                        ],
+                      ),
+                      Row(
                         children: [
-                          Text('Speed : ${pokemon.speed}'),
-                          Text('Total : ${pokemon.total}'),
-                        ])
-                  ],
-                ),
-              )),
-        ));
+                          Expanded(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('HP : ${pokemon.hp}'),
+                                  Text('Atk : ${pokemon.atk}'),
+                                  Text('Sp. Atk : ${pokemon.speAtk}'),
+                                  Text('Speed : ${pokemon.speed}'),
+                                ]),
+                          ),
+                          Expanded(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(''),
+                                  Text('Def : ${pokemon.def}'),
+                                  Text('Sp. Def : ${pokemon.speDef}'),
+                                  Text('Total : ${pokemon.total}'),
+                                ]),
+                          ),
+                        ],
+                      ),
+                    ])),
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
